eb6e8d9714de8ea657021a69b93b134cc8e57f4b
Jopereira2005 <jopereiravieira98@gmail.com>
2024-05-09 21:47:43 -0300

    #

---
+++ System/config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => ['*'],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => ['*'],
+    'supports_credentials' => true,
+    'content_type' => ['application/json']
+];
